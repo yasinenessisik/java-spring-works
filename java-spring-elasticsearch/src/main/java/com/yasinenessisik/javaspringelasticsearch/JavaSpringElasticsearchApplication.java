9a86868b140f9f40e7dda3aa9e3ddcb7f48cf97e
yasinenessisik <128436451+yasinenessisik@users.noreply.github.com>
2024-04-28 16:31:31 +0300

    File created.

---
+++ java-spring-elasticsearch/src/main/java/com/yasinenessisik/javaspringelasticsearch/JavaSpringElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.yasinenessisik.javaspringelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringElasticsearchApplication.class, args);
+	}
+
+}
---
+++ java-spring-elasticsearch/src/test/java/com/yasinenessisik/javaspringelasticsearch/JavaSpringElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasinenessisik.javaspringelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
